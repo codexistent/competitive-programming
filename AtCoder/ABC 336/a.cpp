c10fa345c4e7f22f308449d9a8e08376594c1108
Pearl Yu <77512088+codexistent@users.noreply.github.com>
2024-01-14 10:35:20 -0600

    Create a.cpp

---
+++ AtCoder/ABC 336/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  FOR(i, a, b) for(int i = a; i  <= b; i++)
+
+int main(){
+    int N;
+    cin >> N;
+    cout << "L";
+    FOR(i, 1, N) cout << "o";
+    cout << "ng" << endl;
+}
